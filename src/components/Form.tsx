02bfb6dc2d18ad2764c5afbd0c018e65266c2854
Miguel Rodríguez <miguelrodrii@outlook.com>
2022-07-15 00:59:45 -0500

    added sample form with zod and react hook form integration under primereact

---
+++ src/components/Form.tsx
@@ -0,0 +1,42 @@
+import {Controller, useForm} from 'react-hook-form';
+import {CreateOneTestInput, createOneTestSchema} from '../schemas/test.schema';
+import {zodResolver} from '@hookform/resolvers/zod/dist/zod';
+import {useCallback} from 'react';
+import {createOneTestActionHandler} from '../services/store/actionHandlers/test.actionHandler';
+import {getTestDispatcher} from '../services/store/slices/test.slice';
+import {useStore} from '../services/store/useStore';
+import {CREATE_ONE_TEST_RESTENDPOINT} from '../constants/restEndpoints/test.restEndpoint';
+import {InputText} from 'primereact/inputtext';
+import {Button} from 'primereact/button';
+
+export const Form = () => {
+    const {control, handleSubmit} = useForm<CreateOneTestInput>({
+        resolver: zodResolver(createOneTestSchema),
+        defaultValues: {
+            name: '',
+        },
+    });
+    const onSubmit = useCallback((values: CreateOneTestInput) => {
+        console.log(values);
+        createOneTestActionHandler(getTestDispatcher(useStore))({
+            input: values,
+        })(CREATE_ONE_TEST_RESTENDPOINT);
+    }, []);
+
+    return (
+        <form onSubmit={handleSubmit(onSubmit)}>
+            <Controller
+                name="name"
+                control={control}
+                render={({field, fieldState}) => (
+                    <>
+                        <InputText id={field.name} {...field} autoFocus />
+                        <h1>{fieldState.error?.message}</h1>
+                    </>
+                )}
+            />
+
+            <Button type="submit" label="Submit" />
+        </form>
+    );
+};
--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,16 @@
 /* eslint-disable @typescript-eslint/no-non-null-assertion */
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import App from './App';
+// import App from './App';
 import './index.css';
+
+import 'primereact/resources/themes/lara-light-indigo/theme.css'; //theme
+import 'primereact/resources/primereact.min.css'; //core css
+import 'primeicons/primeicons.css'; //icons
+import {Form} from './components/Form';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
     <React.StrictMode>
-        <App />
+        <Form></Form>
     </React.StrictMode>
 );
