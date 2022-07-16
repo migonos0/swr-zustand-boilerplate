a490b08ab70b435839b090966f08dd461f8a5ad1
Miguel Rodríguez <miguelrodrii@outlook.com>
2022-07-16 18:04:59 -0500

    modified From sample component

--- src/components/Form.tsx
+++ src/components/Form.tsx
@@ -1,13 +1,13 @@
 import {Controller, useForm} from 'react-hook-form';
 import {CreateOneTestInput, createOneTestSchema} from '../schemas/test.schema';
 import {zodResolver} from '@hookform/resolvers/zod/dist/zod';
-import {useCallback} from 'react';
 import {createOneTestActionHandler} from '../services/store/actionHandlers/test.actionHandler';
 import {getTestDispatcher} from '../services/store/slices/test.slice';
 import {useStore} from '../services/store/useStore';
 import {CREATE_ONE_TEST_RESTENDPOINT} from '../constants/restEndpoints/test.restEndpoint';
 import {InputText} from 'primereact/inputtext';
 import {Button} from 'primereact/button';
+import {useCallback} from 'react';
 
 export const Form = () => {
     const {control, handleSubmit} = useForm<CreateOneTestInput>({
@@ -18,9 +18,9 @@ export const Form = () => {
     });
     const onSubmit = useCallback((values: CreateOneTestInput) => {
         console.log(values);
-        createOneTestActionHandler(getTestDispatcher(useStore))({
-            input: values,
-        })(CREATE_ONE_TEST_RESTENDPOINT);
+        createOneTestActionHandler(values)(getTestDispatcher(useStore))(
+            CREATE_ONE_TEST_RESTENDPOINT
+        );
     }, []);
 
     return (
